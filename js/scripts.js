635b33b81151e2bb69f1e34f9d2c7ed591a43c42
Zarina Mazieva <zmazieva78@gmail.com>
2019-10-29 12:33:08 -0700

    add loop function .forEach() to minimize code

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
   $("#form").submit(function(event) {
-    var nameInput = $("#name").val();
-    var lastNameInput = $("#lastName").val();
-    var orderInput = $("#order").val();
-    var dateInput = $("#date").val();
-    var itemInput = $("#item").val();
-    var issueInput = $("#issue").val();
+    var blanks = ["name", "lastName", "order", "date", "item", "issue"];
+    // var nameInput = $("#name").val();
+    // var lastNameInput = $("#lastName").val();
+    // var orderInput = $("#order").val();
+    // var dateInput = $("#date").val();
+    // var itemInput = $("#item").val();
+    // var issueInput = $("#issue").val();
 
-
-    $(".name").text(nameInput);
-    $(".lastName").text(lastNameInput);
-    $(".order").text(orderInput);
-    $(".date").text(dateInput);
-    $(".item").text(itemInput);
-    $(".issue").text(issueInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+    // $(".name").text(nameInput);
+    // $(".lastName").text(lastNameInput);
+    // $(".order").text(orderInput);
+    // $(".date").text(dateInput);
+    // $(".item").text(itemInput);
+    // $(".issue").text(issueInput);
 
     $("#letter").show();
 
